7e8fda9f1d4b8486ef86e0ee99444bd09fc7ed30
Dekrypte0 <majidbeker36@gmail.com>
2026-07-04 13:56:54 +0300

    Enhance iframe attributes for fullscreen support
    Update iframe attributes to include fullscreen options.

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function _getServerIdx() {
 function buildPlayer(container, type, id, season, episode) {
   const idx = _getServerIdx();
   // We cannot use sandbox because video providers detect it and block playback to force their ads.
-  const iframeAttrs = `allowfullscreen allow="autoplay; fullscreen"`;
+  const iframeAttrs = `allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" allow="autoplay; fullscreen"`;
 
   container.innerHTML = `
     <div class="player-server-bar">
